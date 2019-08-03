174e528860d3676177eb4d4ebb8380cb8c1aa2ce
구민회 <inojyes@gmail.com>
2019-08-03 11:32:38 +0900

    add NamingConvention

---
+++ source/NamingConvention.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+/*:
+ # Naming Convention
+ 
+ ## CamelCase
+ 
+ ### UpperCamelCase
+ - 첫번째 문자가 대문자, 나머지 단어의 첫문자는 대문자
+ - 사용되는 부분 : Class, Struct, Enumberation, Extension, Protocol
+ 
+        Name
+        BookIndex
+        MemberNumber
+        CreditCard
+        UserIdentifier
+ 
+ ### lowerCamelCase
+ - 첫번째 문자가 소문자, 나머지 단어의 첫문자는 대문자
+ - 사용되는 부분 : Variable, Constant, Function, Parameter, Property, Method
+ 
+        name
+        bookIndex
+        memberNumber
+        creditCard
+        userIdentifier
+ */
