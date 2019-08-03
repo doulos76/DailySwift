f4d55b76a9830e2f8c4ee5d690d1874ea372c119
구민회 <inojyes@gmail.com>
2019-08-03 11:22:06 +0900

    add VariablesAndConstant

---
+++ source/VariablesAndConstant.playground/Pages/Constant.xcplaygroundpage/Contents.swift
@@ -0,0 +1,21 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/*:
+ # Constants
+ 
+ ## Syntax
+
+    let constantName = initialValue
+ */
+
+let name = "Yan"
+name
+//name = "Brian"
+
+
+// Swift에서는 변수와 상수중 상수를 선호함
+// 상수를 컴파일러가 최적화함
+
+//: [Next](@next)
---
+++ source/VariablesAndConstant.playground/Pages/Variable.xcplaygroundpage/Contents.swift
@@ -0,0 +1,40 @@
+import UIKit
+
+/*:
+ # Variables
+ 
+ ## Syntax
+ 
+    var variableName = initialValue
+ 
+*/
+
+var name = "Swift"
+var year = 2019
+var valid = true
+
+var x = 0.0, y = 0.0, z = 0.0
+
+name
+print(name)
+
+print(year)
+print(valid)
+print("x: \(x), y: \(y), z: \(z)")
+
+/*:
+ ## Syntax
+ 
+    variableName = initialValue
+*/
+name = "Objective-C"
+name = "Java"
+print(name)
+
+var anotherName = name
+anotherName = "Tom"
+print(name, anotherName)
+
+//year = "2019"
+
+//: [Next](@next)
