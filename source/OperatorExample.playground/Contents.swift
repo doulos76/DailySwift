5e6c8ce9104640468667819bf24c750d9a982c39
구민회 <inojyes@gmail.com>
2019-08-18 21:14:10 +0900

    add OperatorExample

---
+++ source/OperatorExample.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+let a: Int = 12
+let b: Int = 34
+let c: Double = Double(a)
+let d: Double = Double(b)
+
+a % b
+
+//c % d
+// 실수의 나머지 연산: truncatingRemainder(dividingBy: )
+c.truncatingRemainder(dividingBy: d)
+
+
+
